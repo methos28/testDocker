7b03242352cc3c693a2e20332cfbff0e7d536265
Methos <25006979+methos28@users.noreply.github.com>
2022-03-11 18:23:29 +0530

    Updated_added time to logger

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import time
+import datetime
 import sys
+now = datetime.now()
 n = int(input("Enter Interval"))
 l = int(input("Enter No. of lines to Show at a time"))
 while n!=0:
     for x in range(l):
-        print ('Log Generation with respect to Time and Size')
+        print ('Log Generation with respect to Time and Size', now)
     n-=1
     time.sleep(1)
