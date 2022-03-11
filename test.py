492c676c2d13f8052b413a5a7904eba1de2977ef
Methos <25006979+methos28@users.noreply.github.com>
2022-03-11 22:46:45 +0530

    Working Code

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 import time
 import datetime
 
-now = datetime.datetime.now()
 n = int(input("Enter Interval"))
 l = int(input("Enter No. of lines to Show at a time"))
-while n!=0:
-    for x in range(l):5
-        print ('Log Generation with respect to Time and Size', now)
-    n-=1
-    time.sleep(1)
+
+for x in range(n):
+    for x in range(l):
+        now = datetime.datetime.now()
+        print("Random Log Generation with time and Size input at", now)  
+
+    time.sleep(1) 
