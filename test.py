6d6ab68cedd12f1c0da13894307308099b60a706
Methos <25006979+methos28@users.noreply.github.com>
2022-03-11 17:16:53 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import sys
 n = int(input("Enter Interval"))
 l = int(input("Enter No. of lines to Show at a time"))
 while n!=0:
-    for x in range(n):
+    for x in range(l):
         print ('Random Log')
     n-=1
     time.sleep(1)
