617b3ee904f6d0039a593a965372511ec703ec59
Methos <25006979+methos28@users.noreply.github.com>
2022-03-11 18:15:30 +0530

    update_test

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ n = int(input("Enter Interval"))
 l = int(input("Enter No. of lines to Show at a time"))
 while n!=0:
     for x in range(l):
-        print ('Log Generation with respect to Time and Size)
+        print ('Log Generation with respect to Time and Size')
     n-=1
     time.sleep(1)
