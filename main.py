0d52dd30f6cdccdb4504404a615fa0a79d97f643
Methos <25006979+methos28@users.noreply.github.com>
2022-03-16 00:18:30 +0530

    API test

---
+++ main.py
@@ -0,0 +1,32 @@
+import flask
+import json
+import time
+
+
+from simplejson import dump
+
+app = flask.Flask (__name__)
+
+@app.route('/', methods = ['GET'])
+def home():
+    dict_home = {'Page':'Home', 'Message':'API Request to home page', 'Timestamp':time.time()}
+    json_dump = json.dumps(dict_home)
+
+    return json_dump
+
+@app.route('/nothome/', methods = ['GET'])
+def nothome():
+    dict_nothome = {'Page':'NotHome', 'Message':'API Request to NotHome page', 'Timestamp':time.time()}
+    json_dump = json.dumps(dict_nothome)
+
+    return json_dump
+
+@app.route('/another/', methods = ['GET'])
+def another():
+    dict_another = {'Page':'Another', 'Message':'API Request to Another page', 'Timestamp':time.time()}
+    json_dump = json.dumps(dict_another)
+
+    return json_dump
+
+if __name__ == '__main__':
+    app.run(port=7777)
